0d9ab0b03ffdc67cdddffc2c2efda9bb2230f3cd
Paloma-commits <pkubusch@ptc.com>
2021-12-15 15:40:48 -0500

    edits to backend

--- udagram-api/src/sequelize.ts
+++ udagram-api/src/sequelize.ts
@@ -1,13 +1,17 @@
 import { Sequelize } from "sequelize-typescript";
 import { config } from "./config/config";
 
-export const sequelize = new Sequelize({
-  username: config.username,
-  password: config.password,
-  database: config.database,
-  host: config.host,
-  //port: config.port,
+// export const sequelize = new Sequelize({
+//   username: config.username,
+//   password: config.password,
+//   database: config.database,
+//   host: config.host,
+//   //port: config.port,
 
-  dialect: "postgres",
-  storage: ":memory:",
-});
+//   dialect: "postgres",
+//   storage: ":memory:",
+// });
+
+export const sequelize = new Sequelize(
+  "postgres://postgres:postgres@database-1.csleevr00ujx.us-east-1.rds.amazonaws.com:5432/postgres"
+);
